230ab7b34b0ff7eebcadb5e946ec79dcaf4d2e09
Lekan Dar <Lekandar11@gmail.com>
2021-11-26 12:09:23 -1200

    Admin-Projec

---
+++ src/components/topbar/Topbar.jsx
@@ -0,0 +1,33 @@
+import React from "react";
+import "./topbar.css";
+import { NotificationsNone, Language, Settings } from "@material-ui/icons";
+
+export default function Topbar() {
+  return (
+    <div className="topbar">
+      <div className="topbarWrapper">
+        <div className="topLeft">
+          <span className="logo">Lekan Dar</span>
+        </div>
+        <div className="topRight">
+          <div className="topbarIconContainer">
+            <NotificationsNone />
+            <span className="topIconBadge">2</span>
+          </div>
+          <div className="topbarIconContainer">
+            <Language />
+            <span className="topIconBadge">2</span>
+          </div>
+          <div className="topbarIconContainer">
+            <Settings />
+          </div>
+          <img
+            src="https://images.pexels.com/photos/1222271/pexels-photo-1222271.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940"
+            alt=""
+            className="topAvatar"
+          />
+        </div>
+      </div>
+    </div>
+  );
+}
